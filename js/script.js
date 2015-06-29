f38cb9620fae0d9825adca89abfb7d24dd686fd7
Alex <alex@MacMini1.local>
2015-06-29 11:24:44 -0400

    Smoother Transition
    When the user scrolls down to a point, the top header shows BUT the navigation bar in the first section is hidden. This is reverse when the user scrolls UP from a certain point.

--- js/script.js
+++ js/script.js
@@ -2,19 +2,24 @@ $(document).ready(function(){
 	//variable holds height of placeholder (soon to be images) on page
 	var imageHeight = $('img').height(); 
 	console.log('hey!');
-	
-	$('#topHeader').hide();	
-	
 
-	document.getElementById('mainHeader').onscroll = function(){
-		console.log('NOW it works');
-	};
+	//hides the topHeader (the stickied header) immediately after the page is finished loading
+	$('#topHeader').hide();	
 
 //uses a jquery plugin to use a Waypoint to...execute an action when a point has been reached.
-	var example = new Waypoint({
+	var removeTopHeader = new Waypoint({
+		element: document.getElementById('endTarget'),
+		handler: function(){
+			$('#topNavigation').show();
+			$('#topHeader').slideUp();
+		}
+	});
+
+	var showTopHeader = new Waypoint({
 		element: document.getElementById('exploreSection'),
 		handler: function(){
-			$('#topHeader').show();
+			$('#topHeader').slideDown();
+			$('#topNavigation').hide();
 		}//end of event handler
 	});//end of waypoint example
 });//when the document is loaded.
