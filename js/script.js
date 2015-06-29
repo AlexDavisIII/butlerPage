c3ae0f963afc44ef99f5f67a9e22a2d174c5100b
Alex Davis III <alex.iii.davis@gmail.com>
2015-06-29 00:11:20 -0400

    Style Overhaul
    Implemented styling design. Used school colors for color coordination
    purposes. Still in unfinished/prototype stages

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
 $(document).ready(function(){
 	//variable holds height of placeholder (soon to be images) on page
-	var imageHeight = $('.placeholder').height(); 
+	var imageHeight = $('img').height(); 
 	console.log(imageHeight);
 
-	var fullImageHeight = imageHeight + 'px';
-    //sets the content holder container to the height of the image it holds
-	$('.contentHolder').css('height', fullImageHeight); 
-	console.log(fullImageHeight);
+	//set the minimum height for all sections available!
 });//when the document is loaded.
