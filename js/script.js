201856a8475a8ee87d83e925edcb573b7259888d
Alex <alex@MacMini1.local>
2015-06-29 10:57:16 -0400

    More Javascript Functionality and Plugin usage
    Used a plugin "Waypoint" to make scrolling manageable.
    Added new section to make use of waypoint
    Made small change to image in parent section!

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
 	//variable holds height of placeholder (soon to be images) on page
 	var imageHeight = $('img').height(); 
-	console.log(imageHeight);
+	console.log('hey!');
+	
+	$('#topHeader').hide();	
+	
 
-	//set the minimum height for all sections available!
+	document.getElementById('mainHeader').onscroll = function(){
+		console.log('NOW it works');
+	};
+
+//uses a jquery plugin to use a Waypoint to...execute an action when a point has been reached.
+	var example = new Waypoint({
+		element: document.getElementById('exploreSection'),
+		handler: function(){
+			$('#topHeader').show();
+		}//end of event handler
+	});//end of waypoint example
 });//when the document is loaded.
